fba4c5fab5cd13121ad3310b5ce05f5da75f3411
Your Name <you@example.com>
2024-11-18 23:33:06 -0300

    first commit

---
+++ assets/js/script2.js
@@ -0,0 +1,10 @@
+function aparecer(){
+    const menu = document.getElementById("lat")
+    menu.classList.remove("aparecer")
+  }
+
+  function remover(){
+    const menu = document.getElementById("lat")
+    menu.classList.add("aparecer")
+  }
+  
